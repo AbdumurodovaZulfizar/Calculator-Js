78dd42636caa8735ea6d98691018d219103319b6
AbdumurodovaZulfizar <abdumurodovazulfizar77@gmail.com>
2021-08-30 12:15:37 +0500

    Add subtract, multiply, divide functions

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,15 @@
 const add = (...args) => {
   return args.reduce((sum, num) => sum + num);
 };
+
+const substract = (x, y) => {
+  return x - y;
+}
+
+const multiply = (...args) => {
+  return args.reduce((mul, num) => mul * num)
+};
+
+const divide = (x, y) => {
+  return x/y;
+}
